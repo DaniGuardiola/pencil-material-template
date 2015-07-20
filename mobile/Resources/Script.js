b8edc6bbf49af27aa7982ae5571ad78925c41ece
Dani Guardiola <contact@daniguardiola.me>
2015-07-20 23:45:26 +0200

    Fixed responsive maps bug

--- mobile/Resources/Script.js
+++ mobile/Resources/Script.js
@@ -105,7 +105,7 @@
                 title: p.querySelector(".Texts .Title").textContent,
                 note: p.querySelector(".Texts .Note") && p.querySelector(".Texts .Note").nextElementSibling ? p.querySelector(".Texts .Note").nextElementSibling.innerHTML : "",
                 src: p.querySelector(".Image img").src,
-                map: p.querySelector(".Image map") ? p.querySelector(".Image map").getAttribute("name") : "",
+                map: p.querySelector(".Image map") ? p.querySelector(".Image map") : "",
                 index: i
             });
 
@@ -142,10 +142,23 @@
     function loadPage(page) {
         var image = document.getElementById("image");
         image.src = page.src;
-        image.setAttribute("usemap", "#" + page.map);
+        image.setAttribute("usemap", "#" + page.map.getAttribute("name"));
 
         var notes = document.getElementById("notes");
         notes.innerHTML = page.note || "<span style=\"color: #9E9E9E;\">There are no notes :)</span>";
+
+        if (!page.mapResize) {
+            try {
+                window.imageMapResize(currentPage.map);
+                var evt = document.createEvent('UIEvents');
+                evt.initUIEvent('resize', true, false, window, 0);
+                window.dispatchEvent(evt);
+            } catch (e) {
+                console.log(e);
+            }
+            page.mapResize = true;
+        }
+
     }
 
     function goToPage(id) {
@@ -297,4 +310,148 @@
     }
 
     window.addEventListener("load", start);
+})();
+
+/*! Image Map Resizer
+ *  Desc: Resize HTML imageMap to scaled image.
+ *  Copyright: (c) 2014-15 David J. Bradshaw - dave@bradshaw.net
+ *  License: MIT
+ */
+
+(function() {
+    'use strict';
+
+    function scaleImageMap() {
+
+        function resizeMap() {
+            function resizeAreaTag(cachedAreaCoords) {
+                function scaleCoord(e) {
+                    return e * scallingFactor[(1 === (isWidth = 1 - isWidth) ? 'width' : 'height')];
+                }
+
+                var isWidth = 0;
+
+                return cachedAreaCoords.split(',').map(Number).map(scaleCoord).map(Math.floor).join(',');
+            }
+
+            var scallingFactor = {
+                width: displayedImage.width / sourceImage.width,
+                height: displayedImage.height / sourceImage.height
+            };
+
+            for (var i = 0; i < areasLen; i++) {
+                areas[i].coords = resizeAreaTag(cachedAreaCoordsArray[i]);
+            }
+        }
+
+        function start() {
+            var
+                displayedWidth = null,
+                displayedHeight = null;
+
+            //WebKit asyncs image loading, so we have to catch the load event.
+            sourceImage.onload = function sourceImageOnLoadF() {
+                displayedWidth = displayedImage.width;
+                displayedHeight = displayedImage.height;
+
+                if ((displayedWidth !== sourceImage.width) || (displayedHeight !== sourceImage.height)) {
+                    resizeMap();
+                }
+            };
+
+            //IE11 can late load this image, so make sure we have the correct sizes (#10)
+            displayedImage.onload = function() {
+                if (null !== displayedWidth && displayedImage.width !== displayedWidth) {
+                    resizeMap();
+                }
+            };
+
+            //Make copy of image, so we can get the actual size measurements
+            sourceImage.src = displayedImage.src;
+        }
+
+        function listenForResize() {
+            function debounce() {
+                clearTimeout(timer);
+                timer = setTimeout(resizeMap, 250);
+            }
+            if (window.addEventListener) {
+                window.addEventListener('resize', debounce, false);
+            } else if (window.attachEvent) {
+                window.attachEvent('onresize', debounce);
+            }
+        }
+
+        function listenForFocus() {
+            if (window.addEventListener) {
+                window.addEventListener('focus', resizeMap, false);
+            } else if (window.attachEvent) {
+                window.attachEvent('onfocus', resizeMap);
+            }
+        }
+
+        function getCoords(e) {
+            // normalize coord-string to csv format without any space chars
+            return e.coords.replace(/ *, */g, ',').replace(/ +/g, ',');
+        }
+
+        var
+        /*jshint validthis:true */
+            map = this,
+            areas = map.getElementsByTagName('area'),
+            areasLen = areas.length,
+            cachedAreaCoordsArray = Array.prototype.map.call(areas, getCoords),
+            displayedImage = document.querySelector('img[usemap="#' + map.name + '"]'),
+            sourceImage = new Image(),
+            timer = null;
+
+        start();
+        listenForResize();
+        listenForFocus();
+    }
+
+
+
+    function factory() {
+        function init(element) {
+            if (!element.tagName) {
+                throw new TypeError('Object is not a valid DOM element');
+            } else if ('MAP' !== element.tagName.toUpperCase()) {
+                throw new TypeError('Expected <MAP> tag, found <' + element.tagName + '>.');
+            }
+
+            scaleImageMap.call(element);
+        }
+
+        return function imageMapResizeF(target) {
+            switch (typeof(target)) {
+                case 'undefined':
+                case 'string':
+                    Array.prototype.forEach.call(document.querySelectorAll(target || 'map'), init);
+                    break;
+                case 'object':
+                    init(target);
+                    break;
+                default:
+                    throw new TypeError('Unexpected data type (' + typeof(target) + ').');
+            }
+        };
+    }
+
+
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') { //Node for browserfy
+        module.exports = factory();
+    } else {
+        window.imageMapResize = factory();
+    }
+
+
+    if ('jQuery' in window) {
+        jQuery.fn.imageMapResize = function $imageMapResizeF() {
+            return this.filter('map').each(scaleImageMap).end();
+        };
+    }
+
 })();
