dbb277c6f9ba5a6e0ea1178bec2779438edf81e7
Ben Winding <ben.winding@gmail.com>
2019-07-12 12:17:10 +0930

    Added desktop template

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 rm -rf build/
 mkdir build
+# Build mobile
 pushd mobile/
 zip -r ../build/pencil-material-template-mobile.zip .
+popd
+# Build desktop
+pushd desktop/
+zip -r ../build/pencil-material-template-desktop.zip .
 popd
